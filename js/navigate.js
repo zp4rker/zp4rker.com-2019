c67eccc94d73af7c1f243dd70f0b4e32355a18ca
zp4rker <me@zp4rker.com>
2019-07-27 09:30:00 +1000

    started on portfolio page

---
+++ js/navigate.js
@@ -0,0 +1,7 @@
+function navigate(elem) {
+    var content = document.querySelector(".content");
+    content.style.animation = "scaleOut ease-in .5s 0s forwards";
+    setTimeout(function() {
+        document.location.assign(elem.dataset.link);
+    }, 500);
+}
